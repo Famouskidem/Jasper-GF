194e2028a024e3a120c597255af0560d967e6cfc
Famouskidem <114438081+Famouskidem@users.noreply.github.com>
2026-04-05 13:12:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 import './src/app.js';
+const { Collection } = require('discord.js');
+client.commands = new Collection();
 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+// Handling interactions
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error!', ephemeral: true });
+  }
+});
